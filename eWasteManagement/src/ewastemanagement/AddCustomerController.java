aadace84a7150501e016008f6096ffbd75f539bf
Zac <zaccannon2003@gmail.com>
2023-06-08 01:50:28 +1000

    add customer to database works

--- eWasteManagement/src/ewastemanagement/AddCustomerController.java
+++ eWasteManagement/src/ewastemanagement/AddCustomerController.java
@@ -4,9 +4,20 @@
  */
 package ewastemanagement;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -18,6 +29,60 @@ public class AddCustomerController implements Initializable {
     /**
      * Initializes the controller class.
      */
+    
+    @FXML
+    private Button btnExit;
+    @FXML
+    private Button btnSave;
+    @FXML
+    private TextField txtFirstName;
+    @FXML
+    private TextField txtLastName;
+    @FXML
+    private TextField txtStrNum;
+    @FXML
+    private TextField txtStreetName;
+    @FXML
+    private TextField txtSuburb;
+    @FXML
+    private TextField txtMobile;
+    @FXML
+    private TextField txtEmail;
+    
+    CustomerModel customer = new CustomerModel();
+    List<Customer> result;
+    Stage stage;
+    Parent root;
+    
+    @FXML
+    public void btnExit(ActionEvent event) {
+        //goes back to the home page
+        try {
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();//node is used to keep the window in the same place, can also alter height and width
+            root = FXMLLoader.load(getClass().getResource("Search.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public void btnSaveListener(ActionEvent event){
+        String firstName = txtFirstName.getText();
+        String lastName = txtLastName.getText();
+        String mobile = txtMobile.getText();
+        String email = txtEmail.getText();
+        int streetNum = Integer.parseInt(txtStrNum.getText());
+        String streetName = txtStreetName.getText();
+        String suburb = txtSuburb.getText();
+        int tipCounter = 2;
+        
+
+        //adds the text to the database
+        int addToList = customer.addCustomer(firstName, lastName, mobile, email, streetNum, streetName, suburb, tipCounter);
+    }
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
--- eWasteManagement/src/ewastemanagement/CustomerModel.java
+++ eWasteManagement/src/ewastemanagement/CustomerModel.java
@@ -29,14 +29,37 @@ public class CustomerModel {
         
          try {
             connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-            selectByName = connection.prepareStatement("");
-            selectByMobile = connection.prepareStatement("");
-            insertNewCustomer = connection.prepareStatement("");
-            updateCustomer = connection.prepareStatement("");
+            //selectByName = connection.prepareStatement("");
+            //selectByMobile = connection.prepareStatement("");
+            insertNewCustomer = connection.prepareStatement("INSERT INTO customer"
+            + "( fName, lName, mobile, email, strNo, strName, suburb, tipsCounter)"
+            + "values (?,?,?,?,?,?,?,?)");
+            //updateCustomer = connection.prepareStatement("");
          }
          catch (SQLException e){
              e.printStackTrace();
              System.exit(1);
          }
+    }
+    public int addCustomer(String firstName, String lastName, String mobile,
+            String email, int streetNum, String streetName, String suburb, int tipCounter){
+        int result = 0;
+        
+        try{
+            insertNewCustomer.setString(1, firstName);
+            insertNewCustomer.setString(2, lastName);
+            insertNewCustomer.setString(3, mobile);
+            insertNewCustomer.setString(4, email);
+            insertNewCustomer.setInt(5, streetNum);
+            insertNewCustomer.setString(6, streetName);
+            insertNewCustomer.setString(7, suburb);
+            insertNewCustomer.setInt(8, tipCounter);
+            
+            result = insertNewCustomer.executeUpdate();
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
+        return result;
     }
 }
