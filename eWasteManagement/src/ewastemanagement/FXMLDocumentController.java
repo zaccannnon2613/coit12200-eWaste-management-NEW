b6defbdf3f8e2a8e0a71d1d48d2c03cff36767ec
Zac <zaccannon2003@gmail.com>
2023-06-07 15:36:15 +1000

    homepage is operational

--- eWasteManagement/src/ewastemanagement/FXMLDocumentController.java
+++ eWasteManagement/src/ewastemanagement/FXMLDocumentController.java
@@ -4,12 +4,19 @@
  */
 package ewastemanagement;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 /**
  *
@@ -18,12 +25,90 @@ import javafx.scene.control.Label;
 public class FXMLDocumentController implements Initializable {
     
     @FXML
-    private Label label;
-    
+    private Button btnAddCustomer;
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private Button btnUpdateCustomer;
+    @FXML
+    private Button btnAddBooking;
+    @FXML
+    private Button btnUpdateBooking;
+    @FXML
+    private Button btnSearch;
+    @FXML
+    private Button btnExit;
+    
+    Stage stage;
+    Parent root;
+    
+     @FXML
+    public void btnAddCustomer(ActionEvent event) {
+        //goes back to the home page
+        try {
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();//node is used to keep the window in the same place, can also alter height and width
+            root = FXMLLoader.load(getClass().getResource("AddCustomer.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+     @FXML
+    public void btnUpdateCustomer(ActionEvent event) {
+        //goes back to the home page
+        try {
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();//node is used to keep the window in the same place, can also alter height and width
+            root = FXMLLoader.load(getClass().getResource("UpdateCustomer.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+     @FXML
+    public void btnAddBooking(ActionEvent event) {
+        //goes back to the home page
+        try {
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();//node is used to keep the window in the same place, can also alter height and width
+            root = FXMLLoader.load(getClass().getResource("AddBooking.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+     @FXML
+    public void btnUpdateBooking(ActionEvent event) {
+        //goes back to the home page
+        try {
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();//node is used to keep the window in the same place, can also alter height and width
+            root = FXMLLoader.load(getClass().getResource("UpdateBooking.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+     @FXML
+    public void btnSearch(ActionEvent event) {
+        //goes back to the home page
+        try {
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();//node is used to keep the window in the same place, can also alter height and width
+            root = FXMLLoader.load(getClass().getResource("Search.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+     @FXML
+    public void btnExit(ActionEvent event) {
+        //goes back to the home page
+        System.exit(1);
     }
     
     @Override
